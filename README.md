ea2814dc263a2a0906a3c482ae06a75c04b6425f
shibamoto <silverbirder@gmail.com>
2021-05-23 21:58:28 +0900

    Add sample code

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # try-rust-2
+
+[とほほのRust入門](https://www.tohoho-web.com/ex/rust.html) を読む。
---
+++ src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let value = 1;
+    let mut value2 = 1;
+    if (value == 1) {
+        value2 = 2;
+    }
+    const MAX_POINT: u32 = 100_000;
+    println!("{} {} {}",value, value2, MAX_POINT);
+}
