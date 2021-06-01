3f98b9a8d205e6924e1d55cf2e6f4c8e90d0e3c6
shibamoto <silverbirder@gmail.com>
2021-06-01 18:46:27 +0900

    📚 learn rust

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -35,6 +35,13 @@ fn main() {
 
     let reference_to_nothing = dangle();
     println!("{}", reference_to_nothing);
+
+    let s = String::from("hello world");
+
+    let word = first_word(&s);
+
+    // s.clear();
+    println!("the first word is: {}", word);
 }
 
 fn takes_ownership(some_string: String) {
@@ -57,4 +64,16 @@ fn dangle() -> String {
     let s = String::from("hello");
 
     s
+}
+
+fn first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
 }
