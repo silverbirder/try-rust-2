4df11f32fd7599707eb46751fd0d1a8bd266f7f2
shibamoto <silverbirder@gmail.com>
2021-06-02 19:31:56 +0900

    📚 learn rust

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -16,6 +16,20 @@ impl Message {
     }
 }
 
+#[derive(Debug)] // すぐに州を点検できるように
+enum UsState {
+    Alabama,
+    Alaska,
+    // ... などなど
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
 fn main() {
     let home = IpAddr::V4(127, 0, 0, 1);
     let loopback = IpAddr::V6(String::from("::1"));
@@ -26,4 +40,43 @@ fn main() {
     let some_number = Some(5);
     let some_string = Some("a string");
     let absent_number: Option<i32> = None;
+
+    let coin: Coin = Coin::Quarter(UsState::Alabama);
+    value_in_cents(coin);
+
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+
+    let some_u8_value = 9;
+    match some_u8_value {
+        1 => println!("one"),
+        3 => println!("three"),
+        5 => println!("five"),
+        7 => println!("seven"),
+        _ => (),
+    }
+
+    if let 9 = some_u8_value {
+        println!("three");
+    }
+}
+
+fn value_in_cents(coin: Coin) -> u32 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        }
+    }
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
 }
