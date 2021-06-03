e6222565845b3ca120687afddb76d60329cafb38
shibamoto <silverbirder@gmail.com>
2021-06-03 18:55:06 +0900

    📚 learn rust

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -4,10 +4,17 @@ mod front_of_house {
     }
 }
 
+// use crate::front_of_house::hosting;
+// use self::front_of_house::hosting;
+use crate::front_of_house::hosting::add_to_waitlist as another_add_to_waitlist;
+use std::io::{self, Write};
+
 pub fn eat_at_restaurant() {
     crate::front_of_house::hosting::add_to_waitlist();
 
-    front_of_house::hosting::add_to_waitlist();
+    another_add_to_waitlist();
+    another_add_to_waitlist();
+    another_add_to_waitlist();
 
     let mut meal = back_of_house::Breakfast::summer("Rye");
     meal.toast = String::from("Wheat");
