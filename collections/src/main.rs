ef82b50fe6a8427829f03acc20096a437c955ff5
shibamoto <silverbirder@gmail.com>
2021-06-03 19:57:24 +0900

    📚 learn rust

---
+++ collections/src/main.rs
@@ -0,0 +1,35 @@
+fn main() {
+    let v: Vec<i32> = Vec::new();
+    let v = vec![1, 2, 3];
+
+    let mut v = Vec::new();
+    v.push(5);
+    v.push(6);
+    v.push(7);
+    v.push(8);
+
+    let v = vec![1, 2, 3, 4, 5];
+    let third: &i32 = &v[2];
+    let third: Option<&i32> = v.get(2);
+
+    let mut v = vec![1, 2, 3, 4, 5];
+    let first = &v[0];
+    // v.push(6);
+    println!("The first element is: {}", first);
+
+    let v = vec![100, 32, 57];
+    for i in &v {
+        println!("{}", i);
+    }
+
+    enum SpreadsheetCell {
+        Int(i32),
+        Float(f64),
+        Text(String),
+    }
+    let row = vec![
+        SpreadsheetCell::Int(3),
+        SpreadsheetCell::Text(String::from("blue")),
+        SpreadsheetCell::Float(10.12),
+    ];
+}
