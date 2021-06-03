0e20ff2f61642a2ffc5893a857e608b94317bc8b
shibamoto <silverbirder@gmail.com>
2021-06-03 23:14:27 +0900

    📚 learn rust

---
+++ errors/dev.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+cargo build
+cargo run
---
+++ errors/src/main.rs
@@ -0,0 +1,58 @@
+use std::fs::File;
+use std::io::{self, ErrorKind, Read};
+
+fn main() {
+    // panic!("crash and burn");
+    // let v = vec![1, 2, 3];
+    // v[99];
+
+    // let f = File::open("hello.txt");
+    // let f = match f {
+    //     Ok(file) => file,
+    //     Err(ref error) if error.kind() == ErrorKind::NotFound => {
+    //         match File::create("hello.txt") {
+    //             Ok(fc) => fc,
+    //             Err(e) => {
+    //                 panic!(
+    //                     //ファイルを作成しようとしましたが、問題がありました
+    //                     "Tried to create file but there was a problem: {:?}",
+    //                     e
+    //                 )
+    //             },
+    //         }
+    //     },
+    //     Err(error) => {
+    //         panic!(
+    //             "There was a problem opening the file: {:?}",
+    //             error
+    //         )
+    //     },
+    // };
+
+    // let f = File::open("hello.txt").unwrap();
+    let f = File::open("hello.txt").expect("Failed to open hello.txt");
+}
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let mut s = String::new();
+
+    File::open("hello.txt")?.read_to_string(&mut s)?;
+
+    Ok(s)
+}
+
+// fn read_username_from_file() -> Result<String, io::Error> {
+//     let f = File::open("hello.txt");
+
+//     let mut f = match f {
+//         Ok(file) => file,
+//         Err(e) => return Err(e),
+//     };
+
+//     let mut s = String::new();
+
+//     match f.read_to_string(&mut s) {
+//         Ok(_) => Ok(s),
+//         Err(e) => Err(e),
+//     }
+// }
